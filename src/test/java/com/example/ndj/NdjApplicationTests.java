3a5363e3d49ab732f2e2df2e5f0fe5270d404ae9
ndj <ehdeld123@naver.com>
2024-04-11 02:45:56 +0900

    init 🔨

---
+++ src/test/java/com/example/ndj/NdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ndj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
