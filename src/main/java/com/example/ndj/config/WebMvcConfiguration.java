81fdc74a47bf8b7d8b801bb9f6192382eb92a5de
ndj <ehdeld123@naver.com>
2024-04-12 11:49:43 +0900

    WebMvcConfiguration Addon

---
+++ src/main/java/com/example/ndj/config/WebMvcConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.ndj.config;
+
+import com.example.ndj.filter.CommonFilter;
+import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+    private static final String[] INCLUDE_PATHS = {
+            "/test/*",
+            "/test2/*"
+    };
+
+
+    @Bean
+    public FilterRegistrationBean<CommonFilter> filterBean() {
+
+        FilterRegistrationBean<CommonFilter> registrationBean = new FilterRegistrationBean<>(new CommonFilter());
+        registrationBean.setOrder(Integer.MIN_VALUE);
+
+        if(activeProfile.equals("local")){
+            registrationBean.addUrlPatterns("/*");
+        }else{
+            registrationBean.setUrlPatterns(Arrays.asList(INCLUDE_PATHS));
+        }
+
+        return registrationBean;
+    }
+
+
+}
